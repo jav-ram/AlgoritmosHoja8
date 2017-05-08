3598d065ed2cbd9c3c6bc27c457230ea1afc5c42
ret16339 <ret16339@uvg.edu.gt>
2017-05-08 12:38:56 -0600

    TreeMapSet

---
+++ src/TreeMapSet.java
@@ -0,0 +1,18 @@
+import java.util.TreeMap;
+
+
+public class TreeMapSet implements WordSet {
+
+	@Override
+	public void add(Word wordObject) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Word get(Word word) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
