a0c6583a9d18a80159bf2e40e5951c4c3e2e4e5e
ret16339 <ret16339@uvg.edu.gt>
2017-05-08 12:06:36 -0600

    Agregue lo de mapeo peroooo todavia no he hecho lo del tree map porque ando leyendo para entender :)

---
+++ src/MapeoFactory.java
@@ -0,0 +1,9 @@
+
+public interface MapeoFactory {
+	/**
+	 * @param tipoMapeo	El tipo de mapeo requerido.
+	 * @return	La instaciacion del mapeo.
+	 */
+	@SuppressWarnings("rawtypes")
+	Mapeo getMapeo(String tipoMapeo);
+}
