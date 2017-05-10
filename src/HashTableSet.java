f81e03973debff884ed2d8aeb2e95e6c69f32726
pablogadhi <pablogadhi@gmail.com>
2017-05-10 08:16:04 -0600

    Orden y Primera mplementacion de Hash

---
+++ src/HashTableSet.java
@@ -0,0 +1,19 @@
+import java.util.Hashtable;
+
+/**
+ * Created by pablo on 5/10/2017.
+ */
+public class HashTableSet implements WordSet {
+
+    private Hashtable base = new Hashtable();
+
+    @Override
+    public void add(Word wordObject) {
+        base.put()
+    }
+
+    @Override
+    public Word get(Word word) {
+        return null;
+    }
+}
